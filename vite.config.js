fe91f69653cb86dc3b9d37a96b431243b485bf2b
Rheyno <rheymoair@gmail.com>
2026-03-01 18:05:24 +0700

    perubahan-8

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path' 
 import sitemapPlugin from 'vite-plugin-sitemap'
 import { fileURLToPath } from 'url'
-import vitePrerender from 'vite-plugin-prerender'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,14 +15,6 @@ export default defineConfig({
     sitemapPlugin({ 
       hostname: 'https://dipdop-three.vercel.app/',
       dynamicRoutes: ['/'] 
-    }),
-    vitePrerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/'],
-      rendererOptions: {
-        maxConcurrentRoutes: 1,
-        renderAfterTime: 500,
-      }
     }),
   ],
   resolve: {
