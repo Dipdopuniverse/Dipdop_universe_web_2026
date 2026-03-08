98b42e3d74423f5975632c7ebf35f8bab4c88a43
Rheyno <rheymoair@gmail.com>
2026-03-08 17:05:58 +0700

    fixing

--- src/section/portofolio.section.jsx
+++ src/section/portofolio.section.jsx
@@ -529,7 +529,7 @@ export function Portofolio() {
                 />
               </div>
             </AnimatedContent>
-            <div className="md:relative absolute top-[50%] md:top-[30%] w-full mb-5 md:w-[50%] flex flex-col gap-6">
+            <div className="md:relative absolute top-[40%] md:top-[30%] w-full mb-5 md:w-[50%] flex flex-col gap-6">
               <AnimatedContent
                 distance={100}
                 direction="vertical"
